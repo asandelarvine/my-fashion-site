1a449f3a5e89151112ef7a1bbd16569b20430b51
Larvine <asandelarvine@gmail.com>
2024-10-30 08:07:34 +0300

    included all files from app dir and components dir

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}', // Include all files in the app directory
+    './components/**/*.{js,ts,jsx,tsx}', // If you have a components directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
